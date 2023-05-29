9524a6c518e99c53a7170f2027ae717f8574b38b
Onsi Gharbi <gharbi.onsi@gmail.com>
2023-05-29 04:19:58 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavouritesPage from './pages/Favourites';
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<AllMeetupsPage />} />
+                <Route path="/new-meetup" element={<NewMeetupPage />} />
+                <Route path="/favourite-meetup" element={<FavouritesPage />} />
+            </Routes>
+        </div>
+    );
 }
 export default App;
